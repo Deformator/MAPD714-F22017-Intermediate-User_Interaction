3e36b71ae85e0c5891c967728b1f4fdf2bacf0c7
Deformator <dammandrii@gmail.com>
2017-09-20 20:27:26 -0400

    Added Outlets and Actions

--- Intermediate-User-Unteraction/ViewController.swift
+++ Intermediate-User-Unteraction/ViewController.swift
@@ -3,18 +3,35 @@
   * Date: September 20, 2017
   * StudentID: 300966307
   * Description: INtermediate User Interaction
-  * Version: 0.1 - Built Basic UI
+  * Version: 0.2 - Added Outlets and Actions
  */
 
 import UIKit
 
 class ViewController: UIViewController {
+    // OUTLETS ++++++++++++++++++++++++++++++
 
+    @IBOutlet weak var nameTextFied: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    // INHERITED METHODS ++++++++++++++++++++
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // ACTIONS METHODS +++++++++++++++++++++++
+    @IBAction func onSliderChange(_ sender: UISlider) {
+    }
 
+    @IBAction func onSegmentedControlChanged(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func doSomethingPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func onSwitchChanged(_ sender: UISwitch) {
+    }
 }
 
